eadfe8aad3ae766380b121f2912f1dab90e1ae89
Jimena Escobar Bermúdez <jime.ebermudez@gmail.com>
2021-10-15 13:57:20 +0000

    solve packages problems

--- src/app.js
+++ src/app.js
@@ -1,11 +1,91 @@
-/* eslint-disable */
-import "bootstrap";
-import "./style.css";
+//import "style.scss";
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
+/**
+ *  EDIT ONLY INSIDE THIS RENDER FUNCTION
+ *  This function is called every time the user changes types or changes any input
+ * 
+    {
+        includeCover: true, // if includeCover is true the algorithm should
+        background: "https://images.unsplash.com/photo-1511974035430-5de47d3b95da", // this is the url of the image that will used as background for the profile cover
+        avatarURL: "https://randomuser.me/api/portraits/women/42.jpg", // this is the url for the profile avatar
+        socialMediaPosition: "left", // social media bar position (left or right)
+        
+        twitter: null, // social media usernames
+        github: null,
+        linkedin: null,
+        instagram: null,
 
+        name: null,
+        lastname: null,
+        role: null,
+        country: null,
+        city: null
+    }
+ */
+function render(variables = {}) {
+  console.log("These are the current variables: ", variables); //print on the console
+  // here we ask the logical questions to make decisions on how to build the html
+  // if includeCover==false then we reset the cover code without the <img> tag to make the cover transparent.
+  let cover = `<div class="cover"><img src="${variables.background}" /></div>`;
+  if (variables.includeCover == false) cover = "<div class='cover'></div>";
+
+  // reset the website body with the new html output
+  document.querySelector("#widget_content").innerHTML = `<div class="widget">
+            ${cover}
+          <img src="${variables.avatarURL}" class="photo" />
+          <h1>Lucy Boilett</h1>
+          <h2>Web Developer</h2>
+          <h3>Miami, USA</h3>
+          <ul class="position-right">
+            <li><a href="https://twitter.com/alesanchezr"><i class="fa fa-twitter"></i></a></li>
+            <li><a href="https://github.com/alesanchezr"><i class="fa fa-github"></i></a></li>
+            <li><a href="https://linkedin.com/alesanchezr"><i class="fa fa-linkedin"></i></a></li>
+            <li><a href="https://instagram.com/alesanchezr"><i class="fa fa-instagram"></i></a></li>
+          </ul>
+        </div>
+    `;
+}
+
+/**
+ * Don't change any of the lines below, here is where we do the logic for the dropdowns
+ */
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  window.variables = {
+    // if includeCover is true the algorithm should
+    includeCover: true,
+    // this is the url of the image that will used as background for the profile cover
+    background: "https://images.unsplash.com/photo-1511974035430-5de47d3b95da",
+    // this is the url for the profile avatar
+    avatarURL: "https://randomuser.me/api/portraits/women/42.jpg",
+    // social media bar position (left or right)
+    socialMediaPosition: "position-left",
+    // social media usernames
+    twitter: null,
+    github: "alesanchezr",
+    linkedin: null,
+    instagram: null,
+    name: null,
+    lastname: null,
+    role: null,
+    country: null,
+    city: null
+  };
+  render(window.variables); //render the card for the first time
+
+  document.querySelectorAll(".picker").forEach(function(elm) {
+    elm.addEventListener("change", function(e) {
+      // <- add a listener to every input
+      const attribute = e.target.getAttribute("for"); // when any input changes, collect the value
+      let values = {};
+      values[attribute] =
+        this.value == "" || this.value == "null"
+          ? null
+          : this.value == "true"
+          ? true
+          : this.value == "false"
+          ? false
+          : this.value;
+      render(Object.assign(window.variables, values)); // render again the card with new valus
+    });
+  });
 };
